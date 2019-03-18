bb79010e00d21d0f70e2212281e2a3d1353c722a
laurencea <laurence.aitchison@gmail.com>
2019-03-18 10:38:24 +0000

    Allowed for HMC nesting

--- bb.py
+++ bb.py
@@ -168,38 +168,48 @@ class HMCTensor():
 
     def record_sample(self, i):
         self.samples[i,...] = self.x_mcmc
+
+    def accept(self):
+        self.x_mcmc.fill_(self.x)
+
+    def refresh_momentum(self):
+        self.p.normal_(0., 1.)
+        self.p.mul_(self.sqrt_mass)
         
 
-class HMC():
-    def __init__(self, fn, vi, chain_length, warmup=0, rate=1E-2, trajectory_length=1.):
-        self.fn = fn
-        self.rate = rate
-        self.steps = int(trajectory_length // rate)
+class HMCDict():
+    def __init__(self, vi, chain_length):
         self.hmcts = {}
-        self.chain_length = chain_length
-        self.warmup = warmup
+        for key, val in vi._modules.items():
+            if isinstance(val, dict):
+                result = HMCDict(val, chain_length)
+            else:
+                result = HMCTensor(val, chain_length)
+            self.hmcts[key] = result
 
-        for k, v in vi.tensors._modules.items():
-            self.hmcts[k] = HMCTensor(v, self.chain_length)
+    def accept(self):
+        for v in self.hmcts.values():
+            v.accept()
 
-    def __getitem__(self, key):
-        return self.hmcts[key].x
+    def record_sample(self, i):
+        for v in self.hmcts.values():
+            v.record_sample(i)
 
     def zero_grad(self):
         for v in self.hmcts.values():
             v.zero_grad()
 
-    def momentum_step(self, rate):
-        self.zero_grad()
-        lp = self.fn(self)
-        lp.backward()
-        for v in self.hmcts.values():
-            v.momentum_step(rate)
-        return lp
-
     def position_step(self, rate):
         for v in self.hmcts.values():
             v.position_step(rate)
+
+    def refresh_momentum(self):
+        for v in self.hmcts.values():
+            v.refresh_momentum()
+
+    def momentum_step(self, rate):
+        for v in self.hmcts.values():
+            v.momentum_step(rate)
 
     def log_prior_xp(self):
         total = 0.
@@ -207,11 +217,42 @@ class HMC():
             total += v.log_prior_xp()
         return total
 
+    def __getitem__(self, key):
+        val = self.hmcts[key]
+        if isinstance(val, HMCTensor):
+            return val.x
+        else:
+            return val
+
+class HMC():
+    def __init__(self, fn, vi, chain_length, warmup=0, rate=1E-2, trajectory_length=1.):
+        self.fn = fn
+        self.rate = rate
+        self.steps = int(trajectory_length // rate)
+        self.chain_length = chain_length
+        self.warmup = warmup
+
+        self.tensors = HMCDict(vi.tensors, chain_length)
+
+    def zero_grad(self):
+        self.tensors.zero_grad()
+
+    def position_step(self, rate):
+        self.tensors.position_step(rate)
+
+    def log_prior_xp(self):
+        return self.tensors.log_prior_xp()
+
+    def momentum_step(self, rate):
+        self.tensors.zero_grad()
+        lp = self.fn(self.tensors)
+        lp.backward()
+        self.tensors.momentum_step(rate)
+        return lp
+
+
     def step(self, i=None):
-        # Randomise momentum
-        for v in self.hmcts.values():
-            v.p.normal_(0., 1.)
-            v.p.mul_(v.sqrt_mass)
+        self.tensors.refresh_momentum()
 
         lp_prior_xp = self.log_prior_xp()
         lp_like     = self.momentum_step(0.5*self.rate)
@@ -231,12 +272,12 @@ class HMC():
 
         #Acceptance
         if t.rand(()) < acceptance_prob:
-            for v in self.hmcts.values():
-                v.x_mcmc.fill_(v.x)
+            self.tensors.accept()
+            #for v in self.hmcts.values():
+            #    v.x_mcmc.fill_(v.x)
 
         if i is not None:
-            for v in self.hmcts.values():
-                v.record_sample(i)
+            self.tensors.record_sample(i)
 
     def run(self):
         for _ in range(self.warmup):
@@ -288,6 +329,6 @@ vi.fit(10**4)
 hmc = HMC(lp, vi, 1000, rate=1E-1, trajectory_length=1.)
 hmc.run()
 
-print(hmc.hmcts["a"].samples.var().item())
-print(hmc.hmcts["b"].samples.var().item())
+print(hmc.tensors.hmcts["a"].samples.var().item())
+print(hmc.tensors.hmcts["b"].samples.var().item())
 
