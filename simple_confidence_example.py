c8810a80d3fe6a6dd81a5af3b148baf00bf28e48
laurencea <aitchisonl@janelia.hhmi.org>
2019-03-22 12:03:25 +0000

    Updates

--- simple_confidence_example.py
+++ simple_confidence_example.py
@@ -1,7 +1,7 @@
 from bb import RV, Model, VI, HMC
 import torch as t
 
-from distributions import Normal, LogGamma, Gamma, Exponential, Beta
+from distributions import Normal, LogGamma, Gamma, Exponential
 from confidence import confidence#, Threshold
 
 t.set_default_tensor_type(t.cuda.FloatTensor)
@@ -14,20 +14,10 @@ class Likelihood(Model):
         self.N = contrast.size(0)
         self.T = contrast.size(1)
         self.scale = Exponential((self.N, 1, 1), rate=1.)
-        self.obs1 = Normal((self.N, self.T, 1), loc= t.relu( contrast), scale=self.scale)
-        self.obs2 = Normal((self.N, self.T, 1), loc=-t.relu(-contrast), scale=self.scale)
-        self.beta = Beta((self.N, 1, 1), alpha=0.5, beta=0.5)
+        self.obs1 = Normal((self.N, self.T, 1), loc=contrast, scale=self.scale)
 
     def forward(self):
-
-        diff = self.obs2() - self.obs1()
-        m1 = diff
-        m2 = t.sign(diff) * t.max(self.obs2(), self.obs1())
-        b = self.beta() 
-
-        f = b * m1 + (1-b)*m2
-       
-        return confidence(f, t.arange(-2, 3.), 5.)
+        return confidence(self.obs2() - self.obs1(), t.arange(-2, 3.), 5.)
 
 class Joint(Model):
     """
@@ -42,7 +32,7 @@ class Joint(Model):
         return self.likelihood().log_prob(obs).sum()
 
 
-contrast = t.randn(10, 5000, 1)
+contrast = t.randn(10, 1000, 1)
 like = Likelihood(contrast)
 obs = like().sample()
 true_latents = like.dump()
@@ -54,5 +44,5 @@ vi = VI(m)
 vi.fit(3*10**4)
 inferred_latents = like.dump()
 
-hmc = HMC(m, 100)
-hmc.run()
+#hmc = HMC(m, 5000)
+#hmc.run()
